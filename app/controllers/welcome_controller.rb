06d531b590e7a1a59c7b2c2f51a293d7f50ea0dc
Andrew Palumbo <apalumbo@optoro.com>
2015-02-18 16:09:14 -0500

    yolo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,23 @@
 class WelcomeController < ApplicationController
 
   # GET /welcome
+  #def index
+  #  @words = Word.order("random()").limit(3)
+  #  @first_word = Word.positive.order("random()").first
+  #  @second_word = Word.negative.where("id != ?", @first_word.id).order("random()").first
+  #  @third_word = Word.negative.where("id not in (?)", [@first_word.id, @second_word.id]).order("random()").first
+  #end
+
   def index
     @words = Word.order("random()").limit(3)
-    @first_word = Word.positive.order("random()").first
-    @second_word = Word.negative.where("id != ?", @first_word.id).order("random()").first
-    @third_word = Word.negative.where("id not in (?)", [@first_word.id, @second_word.id]).order("random()").first
+    @first_word = Word.adverb.order("random()")
+    @second_word = Word.adjective.order("random()")
+    @third_word = Word.noun.where.order("random()")
   end
+
+ # first must be adjective
+ # second must be adjective-or-noun
+ # third must be noun
+ # also if none of these are positive, roll again. maybe sum the bools. could also roll again if sum > 1?
 
 end
--- db/migrate/20150218040419_create_words.rb
+++ db/migrate/20150218040419_create_words.rb
@@ -2,6 +2,7 @@ class CreateWords < ActiveRecord::Migration
   def change
     create_table :words do |t|
       t.string :word
+      t.integer :component # string eval is not the best way to differentiate, but... #yolo and also I'm a noob
       t.boolean :is_positive
       t.timestamps
     end
