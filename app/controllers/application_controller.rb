545346f9f256600318048cd659bf550abe14597c
Andrew Palumbo <apalumbo@optoro.com>
2015-03-17 18:38:58 -0400

    programmer level: yolo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
-after_filter :set_access_control_headers
-def set_access_control_headers
-  headers['Access-Control-Allow-Origin'] = 'http://mighty-lowlands-6344.herokuapp.com/'
-  headers['Access-Control-Allow-Origin'] = 'http://brownbag.herokuapp.com/'
-  headers['Access-Control-Request-Method'] = '*'
-end
-
 class ApplicationController < ActionController::Base
+  after_filter :set_access_control_headers
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://mighty-lowlands-6344.herokuapp.com/'
+    headers['Access-Control-Allow-Origin'] = 'http://brownbag.herokuapp.com/'
+    headers['Access-Control-Request-Method'] = '*'
+  end
+
   layout "swag"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
